7670bece15f44f73f6af6064853b527a54977624
Manuel Jimena García <manuel.jimena29@gmail.com>
2025-03-06 13:17:54 +0100

    update index

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ import AIPage from './pages/AIPage';
 function App() {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors flex flex-col">
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 flex flex-col">
         <Navbar />
         <main className="flex-grow">
           <Routes>
